1718c795875584962017632408749aa2e710f2ba
Orlando Enamorado Rosabal <orlandoenamoradorosabal@gmail.com>
2023-05-24 18:24:30 -0400

    Fix ''

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { GraphQlModule } from "./graph-ql/graph-ql.module";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQlModule } from './graph-ql/graph-ql.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), GraphQlModule],
--- src/graph-ql/graph-ql.resolver.ts
+++ src/graph-ql/graph-ql.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class GraphQlResolver {
